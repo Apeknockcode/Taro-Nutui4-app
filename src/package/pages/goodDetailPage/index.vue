383db3b2d0ef9883eddc3fa1dbc057749b20ea32
Apeknockcode <freeman_top@163.com>
2023-02-14 00:49:14 +0800

    开发商品详情页面

--- src/package/pages/goodDetailPage/index.vue
+++ src/package/pages/goodDetailPage/index.vue
@@ -27,13 +27,37 @@
                 ¥265 - ¥365
             </view>
         </view>
-        <view class="goods-introduce"></view>
+        <view class="goods-introduce">
+            <view class="name">百香果酸乳酪慕斯（木糖醇）</view>
+            <view class="introduce">
+                除了酸味，百香果是世界上已知，充满芳香的水果之一。
+                冰淇淋口感，不同层次的酸与冰凉
+            </view>
+            <view class="support">
+                <view class="support-item">
+                    <Shop3 :size="12" /> 门店自提
+                </view>
+                <view class="support-item">
+                    <Jdl :size="12" /> 商家配送
+                </view>
+                <view class="support-item">
+                    <Clock :size="12" /> 快递
+                </view>
+            </view>
+
+        </view>
     </view>
 </template>
 
 <script lang="ts">
 import { reactive, toRefs } from "vue"
+import { Shop3, Clock, Jdl } from '@nutui/icons-vue-taro';
 export default {
+    components: {
+        Shop3,
+        Clock,
+        Jdl
+    },
     setup() {
         const state = reactive({
             page: 0,
@@ -48,16 +72,20 @@ export default {
 </script>
 
 <style lang="scss">
-.goodsDetails{
-    .goods-mainImage{
+.goodsDetails {
+    width:100vw;
+
+    .goods-mainImage {
         .nut-swiper-item {
             line-height: 150px;
+
             img {
-              width: 100%;
-              height: 100%;
+                width: 100%;
+                height: 100%;
             }
-          }
-          .page {
+        }
+
+        .page {
             position: absolute;
             bottom: 10rpx;
             right: 10rpx;
@@ -68,28 +96,61 @@ export default {
             text-align: center;
             color: #fff;
             font-size: $font26;
-          }
+        }
     }
-    .goods-price{
+
+    .goods-price {
         width: 100vw;
-        height:400rpx;
         padding: 20rpx;
-        .price{
+
+        .price {
             font-size: $fontP36;
-            color:$themesColorOwner;
+            color: $themesColorOwner;
             font-weight: bold;
             display: flex;
             align-items: baseline;
-            .icon{
+
+            .icon {
                 font-size: $font22;
             }
         }
-        .oldPrice{
+
+        .oldPrice {
             font-size: $font22;
             text-decoration: line-through;
             color: $themeTextColor-3;
         }
     }
-}
 
+    .goods-introduce {
+        padding: 0 20rpx 20rpx 20rpx;
+
+        .name {
+            font-size: $font34;
+            font-weight: bold;
+
+        }
+
+        .introduce {
+            font-size: $font28;
+            color: $themeTextColor-3;
+            margin-bottom: 30rpx;
+        }
+    }
+    .support{
+        color: $themesColorOwner;
+        font-size: $font24;
+        display: flex;
+        font-weight: normal;
+        .support-item{
+            margin-bottom: 10rpx;
+            display: flex;
+            align-items: center;
+            margin-right: 20rpx;
+            .nut-icon{
+                margin-right: 4rpx;
+            }
+        }
+    }
+}
 </style>
